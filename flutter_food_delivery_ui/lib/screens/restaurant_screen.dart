311b221cbb4275f2814a10c0b2a8ae89713bb004
alpermulayim <alper.mulayim48@gmail.com>
2023-05-02 16:48:46 +0300

    restaurant screen implement image and back buttons

--- flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
+++ flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 
-class RestaurantScreen extends StatelessWidget {
+class RestaurantScreen extends StatefulWidget {
+  final Restaurant restaurant;
+  RestaurantScreen({this.restaurant});
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
 
-final Restaurant restaurant;
-RestaurantScreen({this.restaurant});
-
+class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(children: [
+        Stack(
+          children: [
+            Image(
+                height: 220.0,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                image: AssetImage(widget.restaurant.imageUrl)),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: (() => Navigator.pop(context)),
+                      iconSize: 30.0,
+                      color: Colors.white,
+                      icon: Icon(Icons.arrow_back_ios_new)),
+                  IconButton(
+                      onPressed: (() {}),
+                      iconSize: 30.0,
+                      color: Colors.red,
+                      icon: Icon(Icons.favorite))
+                ],
+              ),
+            )
+          ],
+        ),
+      ]),
+    );
   }
 }
