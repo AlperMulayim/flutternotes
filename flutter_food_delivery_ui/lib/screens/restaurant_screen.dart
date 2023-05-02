f678b32d0a06ad4765a55b2f73b656c30c9326fb
alpermulayim <alper.mulayim48@gmail.com>
2023-05-02 17:31:58 +0300

    restaurant screen buttons contact and reviews added

--- flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
+++ flutter_food_delivery_ui/lib/screens/restaurant_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
@@ -46,6 +48,64 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
             )
           ],
         ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.restaurant.name,
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+                  ),
+                  Text("2 miles away")
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RatingStars(widget.restaurant.rating),
+                  SizedBox(height: 10.0),
+                  Text(
+                    widget.restaurant.address,
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w200),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 100.0,
+                    child: TextButton(
+                      onPressed: (() {}),
+                      child: Text("Reviews",style: TextStyle(color: Colors.white),)
+                    ),
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                    Container(
+                    width: 100.0,
+                    child: TextButton(
+                      onPressed: (() {}),
+                      child: Text("Contact",style: TextStyle(color: Colors.white),)
+                    ),
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.circular(15.0)),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
       ]),
     );
   }
